958343056bdfde1e8420663b633da42042461837
Daniel Sungwon Lee <danlee333@gmail.com>
2021-04-11 22:16:48 -0700

    singleNum

--- topInterviewQs/array/singleNum.js
+++ topInterviewQs/array/singleNum.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
+  nums.sort()
 
+  for(let i=0; i<nums.length; i++) {
+
+    if (nums[i-1] !== nums[i] && nums[i+1] !== nums[i]) {
+      return nums[i]
+    }
+
+  }
 };
