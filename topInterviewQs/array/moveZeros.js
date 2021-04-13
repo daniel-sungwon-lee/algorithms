c237f0e7cf88af91861de163959af517ca4f542f
Daniel Sungwon Lee <danlee333@gmail.com>
2021-04-12 19:00:05 -0700

    moveZeros

--- topInterviewQs/array/moveZeros.js
+++ topInterviewQs/array/moveZeros.js
@@ -3,5 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
+  let i = 0
 
+  nums.forEach(num => {
+    if (num === 0) {
+      nums.splice(nums.indexOf(num), 1)
+      nums.push(0)
+    }
+  })
+
+  return nums
 };
