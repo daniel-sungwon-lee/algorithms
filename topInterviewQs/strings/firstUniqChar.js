bd0cc0d237056ca5ca7f6250cd01181123673b22
Daniel Sungwon Lee <danlee333@gmail.com>
2021-06-14 12:19:50 -0700

    firstUniqueChar

--- topInterviewQs/strings/firstUniqChar.js
+++ topInterviewQs/strings/firstUniqChar.js
@@ -3,9 +3,18 @@
  * @return {number}
  */
 var firstUniqChar = function (s) {
+  const wordObj = {}
   const wordArray = s.split('')
 
-  for(let i = 0; i<wordArray.length; i++) {
-
+  for (let i =0; i<wordArray.length; i++) {
+    wordObj[wordArray[i]] = i
   }
+
+  for (let x=0; x<wordArray.length; x++) {
+    if (wordObj[wordArray[x]] === wordArray.indexOf(wordArray[x])) {
+      return wordObj[wordArray[x]]
+    }
+  }
+
+  return -1
 };
